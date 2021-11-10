cf6a868fd8f825819444c2cd6195cc08b4da9807
Cavinkumaran M <82271718+Cavinkumaran-m@users.noreply.github.com>
2021-11-10 22:29:27 +0530

    Updating to version 4
    1.Icon bug removed
    2.Celsius sign added
    3.Progress bar added
    4.Extra panel added for the bar

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Desktop;
 import java.awt.Font;
 import java.net.URL;
@@ -13,18 +14,48 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellRenderer;
 
 public class Main {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
-		String[] date_arr = date_reader();
+		//Progress Bar 
+		JProgressBar bar = new JProgressBar();
+		bar.setBounds(100,280,300,30);
+        bar.setBorderPainted(true);
+        bar.setStringPainted(true);
+        bar.setBackground(Color.WHITE);
+        bar.setForeground(new Color(55,44,130));
+        bar.setValue(0);
+        
+        //Panel for Loading Page
+		JPanel loadingScreen = new JPanel();
+		loadingScreen.setForeground(Color.white);
+		loadingScreen.setVisible(true);
+		loadingScreen.setLayout(null);
+		loadingScreen.setBounds(0,0,500,400);
+		loadingScreen.add(bar);
+		
+		String[] date_arr = date_reader();		//Finds today date
+		
+		//All Labels and components that makes up the GUI
 		String month = date_arr[1];
         Integer year = Integer.parseInt(date_arr[2]);
-        ImageIcon img = new ImageIcon("E:\\Tools\\Eclipse\\Weather_Calender\\icon.png");
+        ImageIcon img = new ImageIcon("icon.PNG");
+        ImageIcon bg = new ImageIcon("bg.PNG");
+        JLabel bg_label = new JLabel(bg);
+        bg_label.setVisible(true);
+        bg_label.setBounds(0,0,500,400);
+        loadingScreen.add(bg_label);
 		JFrame frame = new JFrame("CK's Weather Calender");
+		frame.add(loadingScreen);
 		frame.getContentPane().setBackground(new Color(55,44,130));
 		frame.setIconImage(img.getImage());
 		JLabel month_l = new JLabel("Month : " + month);
@@ -60,20 +91,39 @@ public class Main {
         JButton more = new JButton("More Details");
         more.setVisible(true);
         more.setBounds(260,270,150,20);
-       
+        frame.setVisible(true);
         
-        Web_Scrapper Obj = new Web_Scrapper();
+        bar.setString("Loading GUI Components");
+        bar.setValue(10);
+        Thread.sleep(500);
+        
+        frame.add(month_l);
+		frame.add(year_l);
+		frame.add(location);
+		frame.add(high_temp);
+		frame.add(low_temp);      
+        
+		//Creating an instance of User defined Class 
+        Web_Scrapper Obj = new Web_Scrapper(bar);
         location.setText("Location : " + Obj.get_location());
         final ArrayList<String[]> weather_data = Obj.get_weather_data();
         String url = Obj.get_url();
         
+        bar.setString("Extracting Forecast Data");
+        bar.setValue(60);
+        Thread.sleep(500);
+        
+        //Creating Desktop instance to invoke Browser's action later
         Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+        
+        //event handling for the more button
         more.addMouseListener(new java.awt.event.MouseAdapter() {
         	
         	public void mouseClicked(java.awt.event.MouseEvent evt) {
         		
         	    if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
         	        try {
+        	        	//Opens the browser with required url
         	            desktop.browse(new URL(url).toURI());
         	        } catch (Exception e) {
         	            e.printStackTrace();
@@ -83,17 +133,21 @@ public class Main {
         	}
         });
         
+        //Data for the Calendar
         String column[]={"Sun","Mon","Tue", "Wed", "Thu", "Fri", "Sat"};   
         Integer data[][] = table_generator(weather_data);
         
-              
+        bar.setString("Creating the Calendar");
+        bar.setValue(70);
+        Thread.sleep(500);
+        
+        //Creating JTable
         final JTable jt = new JTable(data,column) {
 			private static final long serialVersionUID = 1577662578070990324L;
 			public boolean isCellEditable(int row, int column) {
         		return false;
         	};
         };   
-        
         jt.getTableHeader().setOpaque(false);
         jt.getTableHeader().setBackground(new Color(13,24,82));
         jt.getTableHeader().setForeground(new Color(28,177,152));
@@ -103,12 +157,26 @@ public class Main {
         jt.setRowHeight(30);
         jt.setShowGrid(true);
         jt.setFont(new Font("Serif", Font.BOLD, 18));
-
+        
+        bar.setString("Feeding the Calendar");
+        bar.setValue(80);
+        Thread.sleep(500);
+        
+        //Creating instance of our own defined rendering class
         TableColorCellRenderer renderer = new TableColorCellRenderer();
         jt.setDefaultRenderer(Object.class, renderer);
 
         JScrollPane sp=new JScrollPane(jt);
+        frame.add(sp);
+		frame.add(more);
+        sp.setBounds(20, 50, 445, 203);
+        sp.setVisible(false);
         
+        bar.setString("Feeding the Calendar");
+        bar.setValue(90);
+        Thread.sleep(500);
+        
+        //Action listener for mouse clicks on the cells of the table
         jt.addMouseListener(new java.awt.event.MouseAdapter()
         		{
         	@Override
@@ -117,22 +185,21 @@ public class Main {
         		int row = jt.rowAtPoint(evt.getPoint());
                 int col = jt.columnAtPoint(evt.getPoint());
                 int z = (row * 7) + col;
-                high_temp.setText("High Temp : " + weather_data.get(z)[1]);
-                low_temp.setText("Low Temp : " + weather_data.get(z)[2]);
+                high_temp.setText("High Temp : " + weather_data.get(z)[1] + "C");
+                low_temp.setText("Low Temp : " + weather_data.get(z)[2] + "C");
 
         	}
         		});       
        
-        frame.add(month_l);
-		frame.add(year_l);
-		frame.add(location);
-		frame.add(high_temp);
-		frame.add(low_temp);
-		frame.add(sp);
-		frame.add(more);
-        sp.setBounds(20, 50, 445, 203); 
-        frame.setVisible(true);
+        bar.setString("Loading Done");
+        bar.setValue(100);
+        Thread.sleep(500);
         
+        //Swithcing to the main frame
+        loadingScreen.setVisible(false);
+        sp.setVisible(true);
+
+        //Message box with necessary info
         JOptionPane.showMessageDialog(frame,
         		"1.Turn off any VPN OR Proxy since we trace your IP "
         		+ "for location to give you accurate forecast"
@@ -140,13 +207,14 @@ public class Main {
         		+ "Accuweather.com\n3.Because of Inefficient fund, the author "
         		+ "couldn't afford any Premium API keys,\nSo this app's weather "
         		+ "forecast is limited with forecasted temperatures of "
-        		+ "the month\n\n"
+        		+ "current month\n\n"
         		+ "Regards from The Author,\nCavinKumaran M, CSE, SJIT ",
         		"Thanks For Using This App", JOptionPane.INFORMATION_MESSAGE);       
 	}
 	
 	public static String[] date_reader() {
 		
+		//Returns Current Date, Month and Year
 		LocalDate myObj = LocalDate.now();
 		DateTimeFormatter dateformat = DateTimeFormatter.ofPattern("dd-MMM-yyyy");
         String date = myObj.format(dateformat);
@@ -156,8 +224,9 @@ public class Main {
 	}
 	
 	public static Integer[][] table_generator(ArrayList<String[]> data) {
+		
+		//Assigns values to the data cells of the Calendar table
 		Integer table_data[][] = new Integer[6][7];
-        
         int k = 0;
         try {
 	        for(int i = 0; i < 6; i++) {
@@ -172,4 +241,33 @@ public class Main {
         }
         return table_data;
 	}
+	
+}
+
+//User defined class to make cells to change their color on selection 
+class TableColorCellRenderer implements TableCellRenderer{
+	
+	private static final TableCellRenderer RENDERER = new DefaultTableCellRenderer();
+	@Override
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+			int row, int column) {
+		
+		Component c = RENDERER.getTableCellRendererComponent(table, value
+				, isSelected, hasFocus, row, column);
+	
+		Color color, f_color;
+		if(row == table.getSelectedRow() && column == table.getSelectedColumn()) {
+			color = new Color(255,57,127);
+			f_color = Color.WHITE;
+		} else {
+			color = new Color(19,32,97);
+			f_color = new Color(79,94,171);
+		}
+		c.setForeground(f_color);
+		c.setBackground(color);
+		
+		((JLabel) c).setHorizontalAlignment(SwingConstants.CENTER);
+		return c;
+	}
+
 }
--- src/main/Web_Scrapper.java
+++ src/main/Web_Scrapper.java
@@ -12,6 +12,8 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.stream.Stream;
 
+import javax.swing.JProgressBar;
+
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 import org.jsoup.Jsoup;
@@ -24,31 +26,57 @@ public class Web_Scrapper {
 	String final_page = new String();
 	String[] tech_data = new String[4];
 	
-   public Web_Scrapper() {
+   public Web_Scrapper(JProgressBar bar) throws InterruptedException {
 	   
        try{
+    	   //Reads IP Address
     	   String ip = ip_reader();
+    	   
+    	   bar.setString("Tracing IP Address");
+    	   bar.setValue(20);
+    	   Thread.sleep(500);
+    	   
+    	   //API Connection through free api key provided at accuweather.com
     	   String api_key = "A5tUxWRSLePNOjOTU8ED5e5KshCgIIwb";
     	   String document = Jsoup.connect("http://dataservice.accuweather.com/locations/v1/cities/ipaddress?apikey=" + api_key + "&q=" + ip + "&language=en-us&details=true").ignoreContentType(true).execute().body();
            try{
+        	   //Writes api respond data to a json file 
         	   FileWriter fw = new FileWriter("temp.json");    
         	   fw.write(document);    
-               fw.close();   
+               fw.close(); 
+               
+               bar.setString("Loading Geographical Data");  
+               bar.setValue(30);
+               Thread.sleep(500);
+               
            }
            catch (Exception e) {
         	   System.out.println(e);
            }
            
+           //Parses the temp.json file to extract geographical data
            String[] json_data = json_parser();
            
+           bar.setString("Parsing Geographical Data");
+           bar.setValue(40);
+           Thread.sleep(500);
+           
+           //Web connection to get weather forecast information
            String link = "https://www.accuweather.com/en/" + json_data[0] + "/" + json_data[2] + "/" + json_data[1] + "/weather-forecast/" + json_data[3];
            Document document2 = Jsoup.connect(link).get();
            Elements links = document2.select("a.header-link");
+           
+           //Finds a particular "monthly" button on the webpage to get monthly forecast details
            for (Element link_ : links) {
         	   if(link_.text().equals("Monthly")) {
         		   final_page = "https://www.accuweather.com" + link_.attr("href");
    				}
-           }          
+           }  
+           
+           bar.setString("Fetching Weather Forecasts");
+           bar.setValue(50);
+           Thread.sleep(500);
+           
        }
        catch (IOException ioe) {
     	   System.out.println("Check Your Internet Connection");
@@ -59,10 +87,10 @@ public class Web_Scrapper {
    
    public String ip_reader() {
 	   
+	   //Returns the IP address of the system
 	   String systemipaddress = "";
 	   try
        {
-		   
            URL url_name = new URL("http://bot.whatismyipaddress.com");
            BufferedReader sc =
            new BufferedReader(new InputStreamReader(url_name.openStream()));  
@@ -78,6 +106,7 @@ public class Web_Scrapper {
    
    public String[] json_parser() {
 	   
+	   //Returns Geographical details after parsing the temp.json file
 	   Path path = Paths.get("temp.json");
 	   StringBuilder sb = new StringBuilder();
 	   try (Stream<String> stream = Files.lines(path)) {
@@ -104,12 +133,18 @@ public class Web_Scrapper {
 	   tech_data[2] = city_name;
 	   tech_data[3] = location_key;
 	   
+	   //after parsing, the file is deleted
 	   File f= new File("temp.json");
 	   f.delete();
 	   return tech_data;
 	   }
    
    public boolean isNumeric(String strNum) {
+	   
+	   /* 
+	    * Checks whether the passed value is numeric or not and returns a boolean value according to it... 
+	    * Used for distinguishing between dates and forecast data
+	   */
 	    if (strNum == null) {
 	        return false;
 	    }
@@ -123,21 +158,25 @@ public class Web_Scrapper {
 	}
    
    public String get_location() {
+	   //Returns location data
 	   return tech_data[2];
    }
    
    public String get_url() {
+	   //Returns URL of the page from which the data are obtained
 	   return final_page;
    }
+   
    public ArrayList<String[]> get_weather_data() {
 	   
+	   //Gets html from the final_page and extracts data from it...
+	   //Returns the forecast data
 	   ArrayList<String[]> weather_data = new ArrayList<String[]>();
 	   try {
 		   String f_page = this.final_page;
 		   Document doc = Jsoup.connect(f_page).get();
 		   Elements cal = doc.select("div.monthly-calendar");
 		   
-			
 		   String temp = cal.text();
 		   String[] data = temp.split(" "); 
 		   String[] data2 = new String[3];
